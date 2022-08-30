41bea9c2a4800df3ffa9a6767cf07fbfcb699535
Hammad Zafar Bawara <hammadbawara@gmail.com>
2022-08-30 17:00:26 +0500

    toolbar MainActivity changed

--- app/src/main/java/com/hz_apps/timebasedlocker/MainActivity.java
+++ app/src/main/java/com/hz_apps/timebasedlocker/MainActivity.java
@@ -1,14 +1,15 @@
 package com.hz_apps.timebasedlocker;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.hz_apps.timebasedlocker.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,7 +23,6 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
@@ -30,5 +30,14 @@ public class MainActivity extends AppCompatActivity {
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+        NavigationUI.setupWithNavController(binding.toolbarMainActivity, navController, appBarConfiguration);
+
+        binding.toolbarMainActivity.inflateMenu(R.menu.option_menu_main_acitivity);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 }
